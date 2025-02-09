2989eaf21fd77e79fa825562841682998861f061
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-02-09 18:57:54 +0100

    delete, modify user crud

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -73,3 +73,32 @@ exports.getUser = (req, res, next) => {
             res.status(404).json({ error });
         });
 };
+
+exports.deleteUser = (req, res, next) => {
+    User.findOne({ _id: req.params.id })
+        .then((user) => {
+            if (user._id != req.auth.userId) {
+                res.status(401).json({ message: "Not authorized" });
+            } else {
+                User.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: "User deleted!" }))
+                    .catch((error) => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+exports.modifyUser = (req, res, next) => {
+    delete req.body.userId;
+    User.findOne({ _id: req.params.id })
+        .then((user) => {
+            if (user._id != req.auth.userId) {
+                res.status(401).json({ message: "Not authorized" });
+            } else {
+                User.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+                    .then(() => res.status(200).json({ message: "User modified!" }))
+                    .catch((error) => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => res.status(400).json({ error }));
+};
