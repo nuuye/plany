89c10cc2b6c541f5818b8b9ceaf845d9d0ccd5cb
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-02-10 23:31:41 +0100

    landing page update

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,19 +5,40 @@ import { RiArrowRightLine } from "react-icons/ri";
 import { Boxes } from "../components/background/background-boxes";
 import { useRouter } from "next/router";
 import preview from "../../public/images/preview.png";
+import logo from "../../public/images/planyLogo.png";
+
 import Image from "next/image";
 
 export default function Home() {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [loaded, setLoaded] = useState(false);
 
     return (
         <div className={styles.root}>
             <Boxes allowColors={false} />
+            <div className={styles.headerContainer}>
+                <div className={styles.sidebar}>
+                    <Image className={styles.logo} src={logo} alt="Plany Logo" />
+                    <div className={styles.buttonContainer}>
+                        <Button className={styles.button} colorPalette="teal">
+                            Testimonials
+                        </Button>
+                        <Button className={styles.button} colorPalette="teal">
+                            About
+                        </Button>
+                    </div>
+                </div>
+            </div>
             <div className={styles.mainContainer}>
                 <div className={styles.descriptionContainer}>
                     <h1>Plany – Your Smart Task Manager.</h1>
-                    <h3>Create, edit, delete and organize your work and life effortlessly now</h3>
+                    <h3>
+                        Create, edit, delete, and organize
+                        <br />
+                        <span style={{ color: "teal" }}>your work and life effortlessly</span> now
+                    </h3>
+
                     <Button
                         loading={isLoading}
                         colorPalette="teal"
@@ -31,7 +52,12 @@ export default function Home() {
                     </Button>
                 </div>
                 <div className={styles.imageContainer}>
-                    <Image className={styles.image} src={preview} alt="Isometric illustration" />
+                    <Image
+                        className={`${styles.image} ${loaded ? styles.imageLoaded : ""}`}
+                        src={preview}
+                        alt="Isometric illustration"
+                        onLoad={() => setLoaded(true)}
+                    />
                 </div>
             </div>
         </div>
