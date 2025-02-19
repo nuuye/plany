fdc91265cba739c174672a6778d8adec72629d68
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-02-19 01:27:18 +0100

    fixed method names for router in user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,7 +8,7 @@ router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
 router.post("/checkingEmail", userCtrl.emailCheck);
 router.get("/getUser/:id", userCtrl.getUser);
-router.get("/deleteUser/:id", auth, userCtrl.deleteUser);
-router.get("/modifyUser", auth, userCtrl.modifyUser);
+router.delete("/deleteUser/:id", auth, userCtrl.deleteUser);
+router.put("/modifyUser", auth, userCtrl.modifyUser);
 
 module.exports = router;
